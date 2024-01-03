d3651477007910161692ff3eee36762aa01cb855
YBO <yitzhak851@gmail.com>
2024-01-03 18:32:16 +0200

    Update jason file - Uper case name of the class

--- LoanCalc.java
+++ LoanCalc.java
@@ -1,26 +1,24 @@
-/**
- * this program computes the periodical payment necessary to re-pay a given
- * loan.
- * Computes the periodical payment necessary to re-pay a given loan.
- */
 public class LoanCalc {
 	// declare some few class static variables
 	static double epsilon = 0.001; // The computation tolerance (estimation error)
-	static int iterationCounter; // Monitors the efficiency of the calculation
-	static int n;
-	// sets the loan value data
-	static double loan, rate, payment, periodicalPayment;
+	static int stepCounter; // Monitors the efficiency of the calculation
+	static int n; // number of periods
+	static double loan; // sum of the loan
+	static double rate;  // periodical interest rate (as a percentage)
+	static double payment; // periodical payment
 
 	/**
 	 * Uses a sequential search method ("brute force") to compute an approximation
-	 * of the periodical payment that will bring the ending balance of a loan close
-	 * to 0.
-	 * Given: the sum of the loan, the periodical interest rate (as a percentage),
-	 * the number of periods (n), and epsilon, a tolerance level.
+	 * of the periodical payment that will bring the ending balance 
+	 * of a loan close to 0.
+	 * Given: the sum of the loan (loan), the periodical interest rate (rate),
+	 * the number of periods (n), and epsilon (epsilon), a tolerance level.
 	 */
 	// Side effect: modifies the class variable iterationCounter.
 	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-
+		while (Math.abs(n) > epsilon)) {
+			
+		}
 		return 0;
 	}
 
@@ -47,10 +45,10 @@ public class LoanCalc {
 		// temp = ending balance
 		double temp = loan;
 		for (int i = 1; i <= n; i++) {
-			temp = (temp - periodicalPayment) * (1 + rate / 100);
+			temp = (temp - periodPayment) * (1 + rate / 100);
 			System.out.println("Period " + i + " = " + temp);
 		}
-		return periodicalPayment;
+		return periodPayment;
 	}
 
 	/**
@@ -63,20 +61,21 @@ public class LoanCalc {
 		loan = Double.parseDouble(args[0]);
 		rate = Double.parseDouble(args[1]);
 		n = Integer.parseInt(args[2]);
-		periodicalPayment = Double.parseDouble(args[3]);
+		payment = 10000;
 		// Prints the loan data
 		System.out.println("Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 		endBalance(loan, rate, n, payment);
+		
 		// Computes the periodical payment using brute force search
 		System.out.print("Periodical payment, using brute force: ");
 		System.out.printf("%.2f", bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println();
-		System.out.println("number of iterations: " + iterationCounter);
+		System.out.println("number of iterations: " + stepCounter);
 
 		// Computes the periodical payment using bisection search
 		System.out.print("Periodical payment, using bi-section search: ");
 		System.out.printf("%.2f", bisectionSolver(loan, rate, n, epsilon));
 		System.out.println();
-		System.out.println("number of iterations: " + iterationCounter);
+		System.out.println("number of iterations: " + stepCounter);
 	}
 }
