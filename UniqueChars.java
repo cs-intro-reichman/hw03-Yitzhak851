fc57464048d41a25980096b7aa691dcdd91fe59b
YBO <yitzhak851@gmail.com>
2024-01-03 17:58:14 +0200

    upload UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,34 +1,39 @@
 public class UniqueChars {
-        // declare variables
-        static String str1, str2;
-        static char tempChar;
-        static int i,j;
+    // declare variables
+    static String str1, str2;
 
-    public static void uniqueChars(String str1) {
-        for (i = 0; i < str1.length() ; i++) {
-            tempChar = str1.charAt(i);
-            if (i==0) {
-                str2 += tempChar;
-            } else {
-                checkingDouble(tempChar);
-            }
-        }
-    }
-
-    public static void checkingDouble(char tempChar) {
-        for (j = 0 ; j < str2.length(); j++) {
-            if (str2.charAt(j) == tempChar) {
-                break;
-            } else {
-                str2 += tempChar;
-            }
-        }
-    }
-    public static void main(String[] args) {  
-        str1 = "committe";
+    public static void main(String[] args) {
+        str1 = args[0];
         str2 = "";
-        uniqueChars(str1);
+        uniqueChars();
         System.out.println(str2);
     }
 
+    public static void uniqueChars() {
+        for (int i = 0; i < str1.length(); i++) {
+            int j = 0;
+            boolean flag = true;
+            if (i == 0) {
+                str2 += str1.charAt(i);
+            }
+            if (i > 0) {
+                if (str1.charAt(i) == ' ') {
+                    str2 += str1.charAt(i);
+                } else {
+                    // check if the character is already in the string
+                    while (j < i && j < str2.length()) {
+                        // if the character is already in the string, set flag to false
+                        if (str1.charAt(i) == str2.charAt(j) ) {
+                            flag = false;
+                        }
+                        j++;
+                    }
+                    // if the character is not in the string, add it to the string
+                    if (flag) {
+                        str2 += str1.charAt(i);
+                    }
+                }
+            }
+        }
+    }
 }
