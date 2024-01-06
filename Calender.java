d24741bd9efd3dcdbb0417a1c1ad2c3d5061c7b1
YBO <yitzhak851@gmail.com>
2024-01-06 14:35:06 +0200

    Update Calender.java

--- Calender.java
+++ Calender.java
@@ -1,10 +1,9 @@
-/*  
- * 
- */
+
 public class Calender {
     // Starting the calendar on 1/1/1900
     static int curMonth;
     static int curDay;
+    static int startYear;
     static int curYear;
     static int endYear;
     static int curDayOfWeek; // ==> (2) 1.1.1900 was a Monday
@@ -13,6 +12,7 @@ public class Calender {
     static int nDays; // number of days in the month
 
     public static void main(String args[]) {
+        startYear = 1990;
         curYear = Integer.parseInt(args[0]);
         endYear = curYear + 1;
         advance();
@@ -23,30 +23,45 @@ public class Calender {
      */
     public static void advance() {
         curDayOfWeek = 2;
-        while (curYear <= endYear) {
+        while (startYear < endYear) {
             curMonth = 1;
             while (curMonth <= 12) {
                 curDay = 1;
-                while (curDay <= nDaysInMonth(curMonth, curYear)) {
-                    if (curDayOfWeek <= 7) {
-                        System.out.print(curDay + "/" + curMonth + "/" + curYear);
-                        if ((curDay == 1) && (curDayOfWeek) == 1) {
-                            System.out.print(" Sunday");
-                            curDay++;
-                            curDayOfWeek++;
-                        } else {
-                            curDay++;
-                            curDayOfWeek++;
+                while (curDay <= nDaysInMonth(curMonth, startYear)) {
+                    if (startYear == curYear) {
+                        if (curDayOfWeek <= 7) {
+                            System.out.print(curDay + "/" + curMonth + "/" + startYear);
+                            if (curDayOfWeek == 1) {
+                                System.out.print(" Sunday");
+                                curDay++;
+                                curDayOfWeek++;
+                            } else {
+                                curDay++;
+                                curDayOfWeek++;
+                            }
+                            if (curDayOfWeek > 7) {
+                                curDayOfWeek = 1;
+                            }
                         }
-                        if (curDayOfWeek > 7) {
-                            curDayOfWeek = 1;
+                        System.out.println();
+                    } else {
+                        if (curDayOfWeek <= 7) {
+                            if ((curDay == 1) && (curDayOfWeek) == 1) {
+                                curDay++;
+                                curDayOfWeek++;
+                            } else {
+                                curDay++;
+                                curDayOfWeek++;
+                            }
+                            if (curDayOfWeek > 7) {
+                                curDayOfWeek = 1;
+                            }
                         }
                     }
-                    System.out.println();
                 }
                 curMonth++;
             }
-            curYear++;
+            startYear++;
         }
     }
 
@@ -71,13 +86,13 @@ public class Calender {
      * @param year  - represents the year
      * @return - the number of days in the given month and year
      */
-    private static int nDaysInMonth(int curMonth, int curYear) {
+    private static int nDaysInMonth(int curMonth, int startYear) {
         switch (curMonth) {
             case 1, 3, 5, 7, 8, 10, 12: // January, March, May, July, August, October, and December
                 nDays = 31;
                 break;
             case 2: // February
-                nDays = isLeapYear(curYear) ? 29 : 28;
+                nDays = isLeapYear(startYear) ? 29 : 28;
                 break;
             case 4, 6, 9, 11: // April, June, September, and November
                 nDays = 30;
